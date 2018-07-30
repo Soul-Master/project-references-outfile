4df74a7f7c441e2cfe5f6e4a6406b026e8815723
Soul_Master <Soul_Master@live.com>
2018-07-31 02:26:42 +0700

    Convert to ES6 module

--- cli/entry-point.ts
+++
@@ -1,3 +0,0 @@
-
-showTempConversion();
-showLengthConversion();
--- converter/length.ts
+++ converter/length.ts
@@ -1,23 +1,21 @@
-// Ha! 
+import { leftPad } from "../utils/utils";
+export const FeetPerMeter = 3.28084;
 
-namespace Convert {
-    export const FeetPerMeter = 3.28084;
+export function metersToFeet(m: number) {
+    return m / FeetPerMeter;
+}
+
+export function feetToMeters(f: number) {
+    return f * FeetPerMeter;
+}
+
+// Takes a number like 14 and returns a string like `1' 2"`
+export function formatInches(i: number) {
+    if (i < 12) {
+        return `${i}"`;
+    }
+    const ft = Math.floor(i / 12);
+    i = i - ft * 12;
     
-    export function metersToFeet(m: number) {
-        return m / FeetPerMeter;
-    }
-
-    export function feetToMeters(f: number) {
-        return f * FeetPerMeter;
-    }
-
-    // Takes a number like 14 and returns a string like `1' 2"`
-    export function formatInches(i: number) {
-        if (i < 12) {
-            return `${i}"`;
-        }
-        const ft = Math.floor(i / 12);
-        i = i - ft * 12;
-        return `${ft}' ${Utils.leftPad(i.toString(), 2)}`;
-    }
+    return `${ft}' ${leftPad(i.toString(), 2)}`;
 }
