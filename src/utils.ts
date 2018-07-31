a9fce78219ce790f68a6b0e1e9679fbb0ced9c62
Sumit Tungdenjing <sumit.tungdenjing@thomsonreuters.com>
2018-07-31 18:53:19 +0700

    Add project for VS2017

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,10 @@ export function add(x: number, y: number) {
     return x + y;
 }
 
+export function remove(x: number, y: number) {
+    return x - y;
+}
+
 export const defaultSettings: AppPlatform.Setting = {
     width: 50,
     height: 50
